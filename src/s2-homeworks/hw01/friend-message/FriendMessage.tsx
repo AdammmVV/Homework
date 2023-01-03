85136f292ed5bee5ed103956a5039254d09ae3cb
Adammm <adammm.web@gmail.com>
2023-01-04 00:34:42 +0300

    Type FriendMessage.tsx

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import s from './FriendMessage.module.css'
+import {MessageType} from "../HW1";
 
 // создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
+type FriendMessage = {
+    message: MessageType
+}
+const FriendMessage = (props: FriendMessage) => {
     return (
         <div
             id={'hw1-friend-message-' + props.message.id}
